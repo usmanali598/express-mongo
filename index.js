f8052cab6d614417b4eefd135e962afc6b02e927
Usman Ali <usman_pervaiz@live.com>
2020-04-10 13:32:03 +0300

    running the server

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const mongoose = require('mongoose');
+const connect = require('./model');
+const express = require('express');
+const app = express();
+const path = require('path');
+const expressHandlerbars = require('express-handlebars');
+const bodyparser = require('body-parser');
 
-mongoose.connect('mongodb://localhost:27017/pankki',  { useUnifiedTopology: true, useNewUrlParser: true }, (error) => {
-    if (!error) {
-        console.log('Successful connection!');
-    } else {
-        console.log('error: '+error)
-    }
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+
+app.use('/', (req, res) => {
+    res.send('<h1>Hello</h1>')
+})
+
+app.listen('3000', () => {
+    console.log('app is on 3000')
 })
---
+++ model/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/pankki',  { useUnifiedTopology: true, useNewUrlParser: true }, (error) => {
+    if (!error) {
+        console.log('Successful connection!');
+    } else {
+        console.log('error: '+error)
+    }
+})
+
+const Pankki = require('./pankki.model');
